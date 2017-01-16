8877f5a1ae6352af28fc1e45c13e84d54a62feaf
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2017-01-16 14:52:14 +0330

    Add files via upload
    For Arian, The Great Debugger!

--- main.c
+++ main.c
@@ -4,11 +4,14 @@
 #include <limits.h>
 
 int arrow_counter = 0;
+char *team_name;
 
 void init();
 void start();
+void teamLists();
 void game();
 void print(char *, int, int);
+void printSpace(int, int, int);
 int windowsWindowWidth();
 void arrow(int);
 
@@ -16,6 +19,7 @@ int main()
 {
     init();
     start();
+    teamLists();
     game();
     return 0;
 }
@@ -194,6 +198,51 @@ void game()
     }
 }
 
+void teamLists()
+{
+    FILE *f = fopen("teams\teams.csv", "r");
+    char *team[16];
+    int i, j;
+    for (i = 0; i < 16; i++)
+        fgets(team[i], 100, f);
+    if (arrow_counter == 0)
+    {
+        int counter_temp = 1;
+        while (TRUE)
+        {
+            arrow(16);
+            if(counter_temp != arrow_counter)
+            {
+                system("cls");
+                puts("");
+                print("                           ", 2, 1);
+                print(" Ultimate Football Manager ", 2, 1);
+                print("                           ", 2, 1);
+                puts("");
+                for (i = 0; i < 16; i++)
+                {
+                    if (arrow_counter == i)
+                    {
+                        //printSpace((27 - strlen(team[i])) / 2, 1, 1);
+                        print(team[i], 1, 1);
+                        //printSpace(27 - (27 - strlen(team[i])) / 2, 1, 1);
+                    }
+                    else
+                    {
+                        //printSpace((27 - strlen(team[i])) / 2, 1, 0);
+                        print(team[i], 1, 0);
+                        //printSpace(27 - (27 - strlen(team[i])) / 2, 1, 0);
+                    }
+                }
+            }
+            counter_temp = arrow_counter;
+            int isReturnPressed = GetAsyncKeyState(VK_RETURN) & 0x8000;
+            if (isReturnPressed)
+                break;
+        }
+    }
+}
+
 int windowsWindowWidth(int type)
 {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -240,6 +289,42 @@ void print(char *s, int clr, int align)
     }
 
     printf("%s\n", s);
+    SetConsoleTextAttribute(hConsole, saved_attributes);
+    return;
+}
+
+void printSpace(int n, int clr, int align)
+{
+    const int total_width = windowsWindowWidth(1);
+    const int field_width = (total_width - 27) / 2;
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    int i;
+    if (align)
+    {
+        for (i = 0; i < field_width; i++)
+            printf(" ");
+    }
+    switch (clr)
+    {
+        case 0 :
+            SetConsoleTextAttribute(hConsole, BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY | FOREGROUND_GREEN);
+            break;
+        case 1 :
+            SetConsoleTextAttribute(hConsole, BACKGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+            break;
+        case 2 :
+            SetConsoleTextAttribute(hConsole, BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY / 3| FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+            break;
+    }
+
+    for (i = 0; i < n; i++)
+        printf(" ");
     SetConsoleTextAttribute(hConsole, saved_attributes);
     return;
 }
